e809ad5f2dd67c83c69e5ed4328c0a0b707cb407
hegerjm <hegerjm@yahoo.com>
2015-10-19 21:22:47 -0500

    cachematrix.R
    updated function for assignment #2

--- cachematrix.R
+++ cachematrix.R
@@ -2,6 +2,11 @@
 ## functions do
 
 ## Write a short comment describing this function
+##This function performs the following:
+##1. Sets the value for the matrix
+##2. Gets the value of the matrix
+##3. Set the value of the inverse matrix
+##4. Gets the value of the inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
@@ -12,13 +17,15 @@ makeCacheMatrix <- function(x = matrix()) {
         get <- function() x
         setinv <- function(solve) m <<- solve
         getinv <- function() m
-        matrix(set = set, get = get,
+        list(set = set, get = get,
              setinv = setinv,
              getinv = getinv)
 }
 
 
 ## Write a short comment describing this function
+## The function calculates the inverse matrix of X using the solve function.  Taking advantage of R lexical scoping to see of the inverse matrix has already
+## been cached.  If it is cached simply retreiving the value from a different environment
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
